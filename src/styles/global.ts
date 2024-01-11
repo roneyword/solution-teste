0c17a6f73035d99b9360e240adb7fc7af87d938d
roney <roneyword@gmail.com>
2024-01-11 18:11:08 -0300

    create project

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: #F8F8F9;
+    -webkit-font-smooth: antialiased;
+  }
+
+  body, input, text-area, button{
+    font-family: 'Encode Sans', sans-serif;
+  }
+
+  button{
+    cursor: pointer;
+  }
+`;
